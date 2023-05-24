828993bb20aeea36e47243c00a9021b1ac75335f
BOSS <emirhan.zor@hotmail.com>
2023-05-24 21:51:03 +0300

    Game class comments

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,10 +1,14 @@
 #include "Game.h"
 
+// Game class constructor function
 Game::Game(sf::RenderWindow* window) : window_(window), status(Status::MainMenu)
 {
+    // Loading the map textures
     lives_texture.loadFromFile("../assets/mariohead.png");
     mario_menu.loadFromFile("../assets/mario_menu.png");
     font.loadFromFile("../assets/font.ttf");
+
+    // Initialize scoreboard variables
     scoreboard_.setLives(MARIO_MAX_LIVES);
     scoreboard_.setScore(0);
 }
@@ -47,7 +51,7 @@ void Game::drawBackground()
         }
     }
 
-    // Draw the left Mario lives 
+    // Draw the remaining Mario lives 
     drawGameInfo();
 }
            
@@ -58,7 +62,7 @@ void Game::drawGameInfo()
     sprite.setOrigin(sprite.getLocalBounds().width / 2, sprite.getLocalBounds().height / 2);
     sprite.setScale(0.8, 0.8);
 
-    // Draw lives according to left lives. 
+    // Draw live symbols according to remaining live count 
     for (int i = 1; i <= scoreboard_.getLives(); i++)
     {
         sprite.setPosition(175.f + 40.f * i, 30.f);
@@ -67,6 +71,7 @@ void Game::drawGameInfo()
 
     sprite.setScale(1, 1);
 
+    // Writing score info to the screen
     std::string score("Score: ");
     score += std::to_string(scoreboard_.getScore());
     sf::Text score_text(score, font, 25);
@@ -74,14 +79,14 @@ void Game::drawGameInfo()
     window_->draw(score_text);
 }
 
-// Create a new mario and insert it objects_ array
+// Creating a new mario and inserting it to the objects_ array
 void Game::createMario()
 {
     objects_.emplace_back(new Mario());
 }
 
-// Create given number turtles and instert it objects_ array. 
-// Turtles initialized in map with a delay.  
+// Create a given number of turtles and instert it to the objects_ array. 
+// Turtles initialized in map with a given delay in constructor as a parameter.  
 void Game::createTurtles(int num) 
 {
     for (int i = 0; i < num; i++) 
@@ -113,70 +118,85 @@ void Game::createTurtles(int num)
 
 }
 
+// Main menu drawing
 void Game::mainMenu()
 {
+    // Welcome text writing to the screen
     sf::Text welcome_text("Welcome to the Mario", font, 40);
     welcome_text.setPosition(SCREEN_WIDTH / 2, 100.f);
     welcome_text.setOrigin(welcome_text.getLocalBounds().width / 2, welcome_text.getLocalBounds().height / 2);
     welcome_text.setFillColor(sf::Color::Red);
     welcome_text.setStyle(sf::Text::Bold);
 
+    // Start text writing to the screen
     sf::Text start_text("Press ENTER to Start", font, 30);
     start_text.setPosition(SCREEN_WIDTH / 2, 250.f);
     start_text.setOrigin(start_text.getLocalBounds().width / 2, start_text.getLocalBounds().height / 2);
 
+    // Mario menu image drawing 
     sf::Sprite mario;
     mario.setTexture(mario_menu);
     mario.setScale(2, 2);
     mario.setPosition(SCREEN_WIDTH / 2, 450.f);
     mario.setOrigin(mario.getLocalBounds().width / 2, mario.getLocalBounds().height / 2);
 
+    // Drawing the above textures to the window 
     window_->draw(welcome_text);
     window_->draw(start_text);
     window_->draw(mario);
 }
 
+// Gameover screen drawing function
 void Game::gameOver()
 {
+    // Gameover text setting
     sf::Text game_over("GAME OVER", font, 64);
     game_over.setPosition(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 100.f);
     game_over.setOrigin(game_over.getLocalBounds().width / 2, game_over.getLocalBounds().height / 2);
     game_over.setFillColor(sf::Color::Red);
     game_over.setStyle(sf::Text::Bold);
 
+    // Return menu text setting
     sf::Text return_menu("Press ENTER to Return Main Menu", font, 32);
     return_menu.setPosition(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 100.f);
     return_menu.setOrigin(return_menu.getLocalBounds().width / 2, return_menu.getLocalBounds().height / 2);
     return_menu.setFillColor(sf::Color::White);
     return_menu.setStyle(sf::Text::Bold);
 
+    // Drawing the above text objects to the window
     window_->draw(game_over);
     window_->draw(return_menu);
 }
 
+// Won screen drawing function
 void Game::won()
 {
+    // Won text setting
     sf::Text won("YOU WON", font, 64);
     won.setPosition(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 100.f);
     won.setOrigin(won.getLocalBounds().width / 2, won.getLocalBounds().height / 2);
     won.setFillColor(sf::Color::Green);
     won.setStyle(sf::Text::Bold);
 
+    // To return menu text setting
     sf::Text return_menu("Press ENTER to Return Main Menu", font, 32);
     return_menu.setPosition(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 100.f);
     return_menu.setOrigin(return_menu.getLocalBounds().width / 2, return_menu.getLocalBounds().height / 2);
     return_menu.setFillColor(sf::Color::White);
     return_menu.setStyle(sf::Text::Bold);
 
+    // Drawing above objects to the window
     window_->draw(won);
     window_->draw(return_menu);
 }
 
+// Getter function for the getScore variable
 int Game::getScore()
 {
     return scoreboard_.getScore();
 }
 
+// Getter function for the getLives variable
 int Game::getRemainingLives()
 {
     return scoreboard_.getLives();
@@ -185,8 +205,10 @@ int Game::getRemainingLives()
 // Checks given object is on floor or not
 bool Game::onFloor(Object* object)
 {
-    float x_pixel{};
-    float y_pixel{};
+    float x_pixel{}; // x coordinate as a pixel
+    float y_pixel{}; // y coordinate as a pixel
+    
+    // Calcualate next pixel values to determine on floor state.
     if (object->getVelocityX() > 0)
         x_pixel = object->getPosition().x + object->sprite.getGlobalBounds().width / 2;
     else
@@ -196,10 +218,9 @@ bool Game::onFloor(Object* object)
     else
         y_pixel = object->getPosition().y;
 
+    // Converting pixel coordinates to tile array coordinates
     float tile_x = x_pixel / TileMap::TILE_SIZE;
     float tile_y = y_pixel / TileMap::TILE_SIZE;
-
-    // below functionality can be exttended and added to general collision check TODO
     
     // bottom left tile
     int tile0_x = floor(tile_x);
@@ -209,12 +230,13 @@ bool Game::onFloor(Object* object)
     int tile1_x = ceil(tile_x);
     int tile1_y = ceil(tile_y);
 
+    // Limit the tile values to the frame sizes
     if (tile0_x < 0) tile0_x = 0;
     if (tile0_x > (SCREEN_WIDTH / TileMap::TILE_SIZE) - 1) tile0_x = (SCREEN_WIDTH / TileMap::TILE_SIZE) - 1;
     if (tile1_x > (SCREEN_WIDTH / TileMap::TILE_SIZE) - 1) tile1_x = (SCREEN_WIDTH / TileMap::TILE_SIZE) - 1;
     if (tile0_y < 0) tile0_y = 0; if (tile1_y < 0) tile1_y = 0;
 
-    // take care of the case when index is out of range TODO
+    // Determine onFloor state acording to tile coordinate values
     if ((map_.getTile(tile0_y, tile0_x) == TileType::Floor or map_.getTile(tile1_y, tile1_x) == TileType::Floor or
          map_.getTile(tile0_y, tile0_x) == TileType::Brick or map_.getTile(tile1_y, tile1_x) == TileType::Brick) and object->getVelocityY() > 0)
     {
@@ -223,6 +245,7 @@ bool Game::onFloor(Object* object)
         else if (dynamic_cast<Turtle*>(object) != nullptr)
             object->sprite.setPosition(object->sprite.getPosition().x, tile0_y * TileMap::TILE_SIZE - 20); // consistent movement
 
+        // Set V speeed to 0, to stay above floor
         object->setVelocityY(0);   
         if (dynamic_cast<Mario*>(object) != nullptr && dynamic_cast<Mario*>(object)->isJumping())
         if (dynamic_cast<Mario*>(object) != nullptr &&dynamic_cast<Mario*>(object)->isJumping())
@@ -236,17 +259,22 @@ bool Game::onFloor(Object* object)
 // It is called in every frame. The function updates the every object in the objects_ vector.
 void Game::updateObjects()
 {
+    // Iterate over the objects array
     for (Object* object : objects_)
     {
         auto pos = object->getPosition();
+
+        // Mario object case
         if (dynamic_cast<Mario*>(object) != nullptr)
         {
+            // mario fail state
             if (marioFail(dynamic_cast<Mario*>(object)))
             {
                 restartGame(false);
                 return;
             }
 
+            // Update the mario according to pressed key
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
             {
                 object->jump();
@@ -263,6 +291,7 @@ void Game::updateObjects()
                 object->setPrevHeading(Object::Direction::RIGHT); // remember the direction when stationary
                 if (pos.x + object->getVelocityX() >= SCREEN_WIDTH - 35) object->setVelocityX(0);
             }
+            // No left,right or up key pressed
             else
             {
                 object->update(Object::Direction::FIXED);
@@ -270,8 +299,8 @@ void Game::updateObjects()
         }
         else  // Turtle case
         {
-            // onFloor, checkcollision will be added.
             float _vx = object->getVelocityX();
+            // Update the turtle according to previous velocity
             if (_vx > 0)
                 object->update(Object::Direction::RIGHT);
             else if (_vx < 0)
@@ -280,26 +309,31 @@ void Game::updateObjects()
                 dynamic_cast<Turtle*>(object)->changeDirection();
         }
 
+        // If object not dead, check the onFloor and checkObstacle cases
         if (!object->getIsDead())
         {
             onFloor(object);
             checkObstacle(object);
         }
 
+        // Move the object, after the update, onFloor and obstacle check 
         object->move();
     }
-    checkCollusion();
+    // Check the collision of the two object
+    checkcollision();
 
 }
 
-void Game::checkCollusion()
+// Check the collision of the every two object in the objects array
+void Game::checkcollision()
 {
     size_t left = 0;
     size_t right = 1;
 
+    // Iterate over every object couple
     while (right < objects_.size())
     {
-        checkCollusion(objects_[left], objects_[right]);
+        checkcollision(objects_[left], objects_[right]);
         right++;
 
         if (right == objects_.size())
@@ -310,11 +344,14 @@ void Game::checkCollusion()
     }
 }
 
-void Game::checkCollusion(Object* o1, Object* o2)
+// Checks collision of two given object
+void Game::checkcollision(Object* o1, Object* o2)
 {
+    // If one of the object is dead, no need to check collision
     if (o1->getIsDead() || o2->getIsDead())
         return;
 
+    // Define the collision offset values
     const float collision_x_offset = 10.f;
     const float collision_y_offset = 20.f;
     const float above_collision_offset = 20.f;
@@ -322,6 +359,7 @@ void Game::checkCollusion(Object* o1, Object* o2)
     // collision case between mario and turtle
     if (dynamic_cast<Mario*>(o1) != nullptr)
     {
+        // Get the mario corner points 
         size_t mario_left     =  o1->sprite.getPosition().x - o1->sprite.getLocalBounds().width / 2 + collision_x_offset;
         size_t mario_right    =  o1->sprite.getPosition().x + o1->sprite.getLocalBounds().width / 2 - collision_x_offset;
         size_t mario_top      =  o1->sprite.getPosition().y - o1->sprite.getLocalBounds().height / 2 + collision_y_offset;
@@ -343,9 +381,6 @@ void Game::checkCollusion(Object* o1, Object* o2)
         turtle_bounding.setFillColor(sf::Color::Transparent);
         turtle_bounding.setOutlineColor(sf::Color::White);
         turtle_bounding.setOutlineThickness(1);
-
-        /*window_->draw(mario_bounding);
-        window_->draw(turtle_bounding);*/
 
         if (mario_right < turtle_left || mario_left > turtle_right || mario_bottom < turtle_top || mario_top > turtle_bottom)
         {
@@ -369,13 +404,13 @@ void Game::checkCollusion(Object* o1, Object* o2)
                 o1->fall();
                 scoreboard_.setLives(scoreboard_.getLives() - 1);
             }
-            
         }
     }
 
     // 2 turtles case (surprise effect)
     else
     {
+        // If two turtle meet, they get surprised and change their directions
         if (!dynamic_cast<Turtle*>(o1)->getIsHide() && !dynamic_cast<Turtle*>(o2)->getIsHide() &&  o1->boundingBox().intersects(o2->boundingBox()))
         {
             dynamic_cast<Turtle*>(o1)->setSurprised();
@@ -420,8 +455,11 @@ void Game::checkObstacle(Object* object)
     int tile_x{};
     int tile_y{};
 
+    // If objects not moving, no need to check
     if (object->getVelocityX() == 0 and object->getVelocityY() == 0)
         return;
+
+    // Calculate the next coordinates according to velocity
 
     if (object->getVelocityX() > 0)
     {
@@ -461,11 +499,12 @@ void Game::checkObstacle(Object* object)
         tile_y = y_pixel / TileMap::TILE_SIZE;
     }
 
-
+    // Limit the coordinates
     if (tile_x < 0) tile_x = 0;
     if (tile_x > (SCREEN_WIDTH / TileMap::TILE_SIZE) - 1) tile_x = (SCREEN_WIDTH / TileMap::TILE_SIZE) - 1;
     if (tile_y < 0) tile_y = 0;
 
+    // Checks the tile is empty or not, to define obstacle state
     if (map_.getTile(tile_y, tile_x) != TileType::Empty)
     {
         if (dynamic_cast<Mario*>(object) != nullptr)
@@ -500,8 +539,10 @@ void Game::checkObstacle(Object* object)
     }
 }
 
+// Restart the game
 void Game::restartGame(bool begin)
 {
+    // Initialize objects again
     for (int i = 0; i < objects_.size(); i++)
     {
         if (dynamic_cast<Turtle*>(objects_[i]) != nullptr)
@@ -531,12 +572,14 @@ void Game::restartGame(bool begin)
         objects_[i]->initialize();
     }
 
+    // Sets the scoreboard variables
     if (begin)
         scoreboard_.setLives(3);
 
     scoreboard_.setScore(0);
 }
 
+// returns the mario is failed or not
 bool Game::marioFail(Mario* m)
 {
     if (m->getIsDead() && m->sprite.getPosition().y > SCREEN_HEIGHT * 3)
--- src/Game.h
+++ src/Game.h
@@ -29,17 +29,18 @@ class Game
 public:
 	Game(sf::RenderWindow* window);							// Constructor
 	void drawBackground();									// Draws the background objects
-	void createMario();
-	void createTurtles(int);
-	void updateObjects();
-	void restartGame(bool begin);
-	void mainMenu();
-	void gameOver();
-	void won();
-	int getScore();
-	int getRemainingLives();
-	~Game();
+	void createMario();										// Creates mario
+	void createTurtles(int);								// Crates turtles
+	void updateObjects();									// Update every object in the vector
+	void restartGame(bool begin);							// Restarts the game
+	void mainMenu();										// Draws the main menu screen
+	void gameOver();										// Draws the gameOver screen
+	void won();												//  Draws the won screen
+	int getScore();											// Returns the score the player get
+	int getRemainingLives();								// Returns the remaining lives of the Mario
+	~Game();												// Destructor funtion to delete dynamic allocated objects
 
+	// Enum class for the game status
 	enum class Status
 	{
 		MainMenu,
@@ -51,23 +52,23 @@ public:
 	Status status;
 
 private:
-	void drawGameInfo();
+	void drawGameInfo();									// Draws the game info
 	bool onFloor(Object* object);							// Checks if object is touching a floor surface
-	void checkCollusion(Object* o1, Object* o2);			// Checks if Mario has hit a turtle and from which side
-	void checkCollusion();
-	void checkObstacle(Object* object);
-	bool marioFail(Mario* m);
+	void checkcollision(Object* o1, Object* o2);			// Checks if Mario has hit a turtle and from which side, or turtles are meet each other
+	void checkcollision();									// Check collision of the every object couple
+	void checkObstacle(Object* object);						// Checks the object will encounter with obstacle or not
+	bool marioFail(Mario* m);								// Returns the mario fail state
 
-	sf::RenderWindow* window_;
-	TileMap map_{};
-	ScoreBoard scoreboard_{};
-	std::vector<Object*> objects_{};
+	sf::RenderWindow* window_;								// Game window object
+	TileMap map_{};											// TileMap class, to store map 2D array
+	ScoreBoard scoreboard_{};								// Score board object
+	std::vector<Object*> objects_{};						// Objects array, which contain all objects in the game
 	sf::Sprite sprite;										// Sprite for general game menu
-	sf::Texture lives_texture;
-	sf::Texture mario_menu;
-	sf::Font font;
+	sf::Texture lives_texture;								// Lives symbol texture
+	sf::Texture mario_menu;									// Mario menu symbol texture
+	sf::Font font;											// Game text fonts
 
-	const size_t MARIO_MAX_LIVES = _MARIO_MAX_LIVES;
+	const size_t MARIO_MAX_LIVES = _MARIO_MAX_LIVES;		// Mario max lives constant value
 };
 
 
